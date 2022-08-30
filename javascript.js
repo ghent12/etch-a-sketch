8690ebc726dd999429ba2190ef4c40ea8c39c9dd
Patrick Davey <ghent12@gmail.com>
2022-08-30 15:44:41 -0500

    Add button to input grid size--not working yet

--- javascript.js
+++ javascript.js
@@ -1,47 +1,41 @@
 let gridUnitsWide = 16
-  , gridUnitsHigh = 16
-  , squareWidth = 20
+  , gridUnitsHigh = gridUnitsWide
+  , padding = 2
+  , totalWidth = 960
+  , squareWidth = (totalWidth - (padding * 2)) / gridUnitsWide
   , squareHeight = squareWidth;
 
 const playArea = document.getElementById('container');
+playArea.style.cssText = "border-style: solid; border-color: black; border-width: 2px; padding: 2px; display: flex; flex-wrap: wrap; width: " + totalWidth + "px; height: auto; align-items: center; justify-items: space-between;"
 
-playArea.style.cssText = "border-style: solid; border-color: black; border-width: 2px; padding: 2px; display: flex; flex-wrap: wrap; width: " + (squareWidth * (gridUnitsWide)) + "px; height: auto; align-items: center; justify-items: space-between;"
+const sizeButton = document.getElementById('square-count-btn');
+sizeButton.addEventListener('click', askGridSize)
 
-function makeGrid() {
-  for (i = 0; i < gridUnitsHigh; i++) {
-    for (j = 0; j < gridUnitsWide; j++) {
+function askGridSize() {
+  gridUnitsWide = prompt("How many squares in each width/height? (Max 100)");
+  gridUnitsHigh = gridUnitsWide;
+  clearGrid();
+  makeGrid(gridUnitsWide);
+}
+
+function clearGrid() {
+  let allSquares = Array.from(document.getElementsByClassName('.play-square'));
+  allSquares.forEach(div => div.remove());
+}
+
+function makeGrid(gridUnitCount) {
+  for (i = 0; i < gridUnitCount; i++) {
+    for (j = 0; j < gridUnitCount; j++) {
       let playSquare = playArea.appendChild(document.createElement('div'))
       playSquare.classList.add("play-square", ("row-" + (j + 1)), ("column-" + (i + 1)));
       playSquare.style.cssText = "width: " + squareWidth + "px; height: " + squareHeight + "px; flex-shrink: 1";
+      playSquare.addEventListener('mouseover', addHoveredClass);
     }
   }
 }
 
-makeGrid();
-
-/*
-  function removeTransition(e) {
-    if (e.propertyName !== 'transform') return; // skip it
-    
-    console.log(e.propertyName);
-    this.classList.remove('playing');
-  }
-
-  const keys = document.querySelectorAll('.key');
-  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-*/
-
-
-const playSquareSelect = Array.from(document.getElementsByClassName('.play-square'));
-
-//[].forEach.call()
-
-//const testSquares = Array.prototype.filter.call(playSquareSelect, (playSquareObj) => playSquareObj.nodeName === "DIV")
-playSquareSelect.forEach(square => square.addEventListener('mouseover', addHoveredClass))
-
-console.log(playSquareSelect)
+makeGrid(gridUnitsWide);
 
 function addHoveredClass(e) {
-  console.log(e);
   this.classList.add('hovered-square');
 }
