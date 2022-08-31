5a96d2c4c7183f65e1b21aedd1b1c17115ddf135
Patrick Davey <ghent12@gmail.com>
2022-08-31 12:56:38 -0500

    Complete basic Etch-a-Sketch

--- javascript.js
+++ javascript.js
@@ -2,8 +2,17 @@ let gridUnitsWide = 16
   , gridUnitsHigh = gridUnitsWide
   , padding = 2
   , totalWidth = 960
-  , squareWidth = (totalWidth - (padding * 2)) / gridUnitsWide
-  , squareHeight = squareWidth;
+  , squareWidth = 0
+  , squareHeight = squareWidth
+  , colorRedValue = Math.round(Math.random() * 2550)/10;
+  , colorGreenValue = Math.round(Math.random() * 2550)/10
+  , colorBlueValue = Math.round(Math.random() * 2550)/10;
+
+  console.group('color-values');
+    console.log('R: ' + colorRedValue);
+    console.log('G: ' + colorGreenValue);
+    console.log('B: ' + colorBlueValue);
+  console.groupEnd('color-values');
 
 const playArea = document.getElementById('container');
 playArea.style.cssText = "border-style: solid; border-color: black; border-width: 2px; padding: 2px; display: flex; flex-wrap: wrap; width: " + totalWidth + "px; height: auto; align-items: center; justify-items: space-between;"
@@ -19,11 +28,20 @@ function askGridSize() {
 }
 
 function clearGrid() {
-  let allSquares = Array.from(document.getElementsByClassName('.play-square'));
-  allSquares.forEach(div => div.remove());
+  //let allSquares = Array.from(document.querySelectorAll('play-square'));
+  //allSquares.forEach(div => div.remove());
+  let allSquares = document.getElementById('container').childNodes;
+  console.log(allSquares.length);
+  allSquares = document.getElementById('container').childNodes;
+  for (k = allSquares.length - 1; k >= 0 ; k--) {
+    allSquares[k].remove();
+  }
+  console.log(allSquares.length);
 }
 
 function makeGrid(gridUnitCount) {
+  squareWidth = (totalWidth - (padding * 2)) / gridUnitCount;
+  squareHeight = squareWidth;
   for (i = 0; i < gridUnitCount; i++) {
     for (j = 0; j < gridUnitCount; j++) {
       let playSquare = playArea.appendChild(document.createElement('div'))
@@ -32,9 +50,15 @@ function makeGrid(gridUnitCount) {
       playSquare.addEventListener('mouseover', addHoveredClass);
     }
   }
+  document.querySelectorAll("play-square").forEach((square) => {
+    square.style.width = squareWidth;
+    square.style.height = squareHeight;
+  })
 }
 
-makeGrid(gridUnitsWide);
+document.addEventListener('load', makeGrid(gridUnitsWide));
+
+//makeGrid(gridUnitsWide);
 
 function addHoveredClass(e) {
   this.classList.add('hovered-square');
